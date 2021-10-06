ec651bea7c9e69fd56addc5ddd2f572cabc96c37
charlottel91 <charlotte.leconte91@gmail.com>
2021-10-06 17:35:07 +0200

    Add button to export data.

--- src/component/UsersTable.js
+++ src/component/UsersTable.js
@@ -6,7 +6,6 @@ import {
   TableCell,
   TableContainer,
   TableHead,
-  TablePagination,
   TableRow,
   Paper,
 } from '@material-ui/core';
@@ -22,7 +21,8 @@ const columns = [
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
+    width: '90%',
+    margin: 'auto',
   },
   container: {
     maxHeight: 440,
@@ -31,56 +31,34 @@ const useStyles = makeStyles({
 
 export default function UsersTable({users}) {
   const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   return (
     <Paper className={classes.root}>
       <TableContainer className={classes.container}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell key={column.id} align={column.align}>
+            <TableRow style={{backgroundColor: 'black'}}>
+              {columns.map((column, i) => (
+                <TableCell key={i} align={column.align}>
                   {column.label}
                 </TableCell>
               ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {users.map((user, i) =>
-              user.guests.map((guest) => (
-                <TableRow key={i}>
-                  <TableCell>{user.id}</TableCell>
-                  <TableCell key={i}>{guest.firstname}</TableCell>
-                  <TableCell key={i}>{guest.lastname}</TableCell>
-                  <TableCell key={i}>{guest.isChild}</TableCell>
-                  <TableCell key={i}>{guest.isVegetarian}</TableCell>
-                  <TableCell key={i}>{guest.presentBrunch}</TableCell>
-                </TableRow>
-              ))
-            )}
+            {users.map((user, i) => (
+              <TableRow key={i}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.firstname}</TableCell>
+                <TableCell>{user.lastname}</TableCell>
+                <TableCell>{user.isChild}</TableCell>
+                <TableCell>{user.isVegetarian}</TableCell>
+                <TableCell>{user.presentBrunch}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={users.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
     </Paper>
   );
 }
